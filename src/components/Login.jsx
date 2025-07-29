def387022e1d7ef3d011b6747c2290a060f50d89
kunal <kashyap.kunal123@gmail.com>
2025-07-29 07:52:21 +0530

    adding email validaton for email using  useRef on submission

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,11 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
   //another way to fetch the values of form
   const emailRef = useRef();
   const passwordRef = useRef();
+
+  const [invalidEmail, setInValidEmail] = useState(false);
 
   function handleSubmitEvent(event) {
     event.preventDefault();
@@ -11,6 +13,16 @@ export default function Login() {
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
     console.log(email, password);
+
+    const isInValidEmail = !email.includes("@");
+    if (isInValidEmail) {
+      setInValidEmail(true);
+      return;
+    }
+
+    setInValidEmail(false);
+
+    console.log("Sending HTTP request");
 
     //reseting form after sumbission
     event.target.reset();
@@ -23,12 +35,20 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" ref={emailRef}/>
+          <input id="email" type="email" name="email" ref={emailRef} />
+          <div className="control-error">
+            {invalidEmail && <p>enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" ref={passwordRef}/>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            ref={passwordRef}
+          />
         </div>
       </div>
 
