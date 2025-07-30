b5acb96a83f77d819b89bd640600ee29e7595052
kunal <kashyap.kunal123@gmail.com>
2025-07-30 07:40:08 +0530

    adding built-in validation to signup form and again adding custom validtion together

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 export default function Signup() {
+  const [passwordMatching, setPasswordMatching] = useState(true);
   //getting values via form data
   function handleFormData(event) {
     event.preventDefault();
@@ -10,12 +13,18 @@ export default function Signup() {
 
     // Another approch is to create a keyvalue pair of each input element and its value
     const data = Object.fromEntries(fd.entries());
-    console.log(data);
+    // console.log(data);
 
     //but in the above approach we wont be able to get the values under fieldset so for those we can manually
     // pull those and set to data
     const acquisition = fd.getAll("acquisition");
     data.acquisition = acquisition;
+
+    // mixing custom validation while using the built-in validation
+    if (data.password !== data["confirm-password"]) {
+      setPasswordMatching(false);
+      return;
+    }
 
     console.log("logging after adding acquisition ");
     console.log(data);
@@ -31,13 +40,20 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input id="email" type="email" name="email" required />
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            //buit-in validator provided by browser
+            required
+            minLength={5}
+          />
         </div>
 
         <div className="control">
@@ -47,6 +63,9 @@ export default function Signup() {
             type="password"
             name="confirm-password"
           />
+          <div className="control-error">
+            {!passwordMatching && <p>confirm password is not same</p>}
+          </div>
         </div>
       </div>
 
@@ -55,18 +74,18 @@ export default function Signup() {
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" />
+          <input type="text" id="first-name" name="first-name" required />
         </div>
 
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" />
+          <input type="text" id="last-name" name="last-name" required />
         </div>
       </div>
 
       <div className="control">
         <label htmlFor="phone">What best describes your role?</label>
-        <select id="role" name="role">
+        <select id="role" name="role" required>
           <option value="student">Student</option>
           <option value="teacher">Teacher</option>
           <option value="employee">Employee</option>
@@ -105,8 +124,13 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
-          agree to the terms and conditions
+          <input
+            type="checkbox"
+            id="terms-and-conditions"
+            name="terms"
+            required
+          />
+          I agree to the terms and conditions
         </label>
       </div>
 
